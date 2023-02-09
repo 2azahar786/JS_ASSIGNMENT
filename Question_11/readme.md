e73efa6b057235a1d5e821ec7809a9fd94c6c3c1
2azahar786 <2azahar786@gmail.com>
2023-02-09 08:36:55 +0530

    third commit

---
+++ Question_11/readme.md
@@ -0,0 +1,14 @@
+# FULL STACK JAVASCRIPT BOOTCAMP 2.0
+
+## JAVASCRIPT ASSIGNMENT
+
+**Question_No.11**
+
+>11. Use the Date object to do the following activities
+    - What is the year today?
+    - What is the month today as a number?
+    - What is the date today?
+    - What is the day today as a number?
+    - What is the hours now?
+    - What is the minutes now?
+    - Find out the numbers of seconds elapsed from January 1, 1970 to now.
---
+++ Question_11/script.js
@@ -0,0 +1,29 @@
+let date = new Date();
+
+let year = date.getFullYear();
+let month = date.getMonth();
+let cdate = date.getDate();
+let day = date.getUTCDay();
+let hour = date.getHours();
+let minut = date.getMinutes();
+
+
+// What is the year today?
+console.log(year);
+
+// What is the month today as a number?
+console.log(month);
+
+// What is the date today?
+console.log(cdate);
+
+// What is the day today as a number?
+console.log(day);
+
+// What is the hours now?
+console.log(hour);
+
+//  What is the minutes now?
+console.log(minut);
+
+
