3e530d5cb90df867968cfc2df12b54c30372c5c0
2azahar786 <2azahar786@gmail.com>
2023-02-24 15:39:44 +0530

    7th commit

---
+++ Question_15/readme.md
@@ -0,0 +1,12 @@
+# FULL STACK JAVASCRIPT BOOTCAMP 2.0
+
+## JAVASCRIPT ASSIGNMENT
+
+**Question_No.15**
+
+>15.  Write a program which can give grades to students according to theirs scores:
+    - 80-100, A
+    - 70-89, B
+    - 60-69, C
+    - 50-59, D
+    - 0-49, F
--- Question_15/script.js
+++ Question_15/script.js
@@ -1,17 +1,31 @@
-let x = n;
 
-for (n=0; n<=100; n++){
-    n = 0+n;
-   return n;
-}
-
-if (n >=80){
-    console.log("70");
+function findgrade(N){
+if (N >= 80){
     console.log("Grade : A");
 }
-else if( n>=70) {
+else if( N >= 70) {
    console.log("Grade : B");
 }
-else{
-    console.log("fails");
+else if(N >= 60){
+    console.log("Grade : C");
+}else if(N >=50){
+    console.log("Grade : D");
+}else{
+    console.log("Grade : F");
 }
+}
+
+// let N = 45; // Grade : F
+// findgrade(N)
+
+// let N = 55; //Grade : D
+// findgrade(N);
+
+// let N = 65;  //Grade : C
+// findgrade(N);
+
+// let N = 75; // Grade : B
+// findgrade(N);
+
+// let N = 85;  // Grade : A
+// findgrade(N);
